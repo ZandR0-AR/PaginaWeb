bd5edd4c9f7a1c710c614be94adb170171e5e118
ZandR0-AR <lizandroarcos321@gmail.com>
2025-10-03 21:25:01 -0500

    Portafolio - Debugging

--- projects/script.js
+++ projects/script.js
@@ -105,9 +105,6 @@ document.onkeydown = function (e) {
     if (e.keyCode == 123) {
         return false;
     }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-        return false;
-    }
     if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
         return false;
     }
