ab55935e87f254bfdee20f492aaa36e4988794a6
ZandR0-AR <lizandroarcos321@gmail.com>
2025-10-03 20:23:22 -0500

    Portafolio Version 2.0

---
+++ assets/js/particles.js
@@ -0,0 +1,34 @@
+ particlesJS("particles-js", {
+        "particles": {
+            "number": {
+            "value": 150
+            },
+            "color": {
+            "value": "#000000"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                "width": 1,
+                "color": "#000000"
+                }
+            },
+            "opacity": {
+            "value": 0.5
+            },
+            "size": {
+            "value": 3
+            },
+            "move": {
+            "enable": true,
+            "speed": 3
+            },
+            "line_linked": {
+                "enable": true,  
+                "distance": 150, 
+                "color": "#000000",
+                "opacity": 0.6,
+                "width": 1
+                }
+        }
+        });
